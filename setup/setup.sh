b5c32dbb0ea5fc9028c45d64c5775d90d9d442be
Steve Blamey <steve.blamey@adbury.net>
2018-11-27 13:37:55 +0000

    Fix-ups for python 3 and postgres 10

--- setup/setup.sh
+++ setup/setup.sh
@@ -7,22 +7,22 @@ ADMINPASS="admin"
 
 # Install prequisites
 sudo apt-get update
-sudo apt-get -y install build-essential libssl-dev libffi-dev python-dev python-pip libsasl2-dev libldap2-dev
-sudo apt-get -y install python-virtualenv
+sudo apt-get -y install build-essential libssl-dev libffi-dev python3-dev python-pip libsasl2-dev libldap2-dev
+sudo apt-get -y install python3-venv
 sudo apt-get -y install postgresql
 
 # postgresql configs
-sudo cp /vagrant/setup/postgresql.conf /etc/postgresql/9.5/main/postgresql.conf
-sudo cp /vagrant/setup/pg_hba.conf /etc/postgresql/9.5/main/pg_hba.conf
+sudo echo "listen_addresses = '*'" >> /etc/postgresql/10/main/postgresql.conf
+sudo cp /vagrant/setup/pg_hba.conf /etc/postgresql/10/main/pg_hba.conf
 sudo systemctl restart postgresql.service
 
 # Create a virtualenv and install superset and requirements
 cd ~
-virtualenv env
+python3 -m venv ./env
 source env/bin/activate
 pip install --upgrade setuptools pip
 pip install superset
-pip install psycopg2
+pip install psycopg2-binary
 
 # Create a postgres role and database
 sudo -u postgres psql -c "CREATE ROLE ${DBROLE} WITH LOGIN PASSWORD '${DBPASS}'"
@@ -30,8 +30,8 @@ sudo -u postgres createdb -O $DBROLE $DBNAME
 
 # Create supset config and set its location
 cp /vagrant/setup/superset_config.py ~/superset_config.py
-echo "export SUPERSET_CONFIG_PATH=/home/ubuntu/superset_config.py" >> ~/.bashrc
-export SUPERSET_CONFIG_PATH=/home/ubuntu/superset_config.py
+echo "export SUPERSET_CONFIG_PATH=/home/vagrant/superset_config.py" >> ~/.bashrc
+export SUPERSET_CONFIG_PATH=/home/vagrant/superset_config.py
 
 # Set-up the superset database
 fabmanager create-admin --app superset --username $ADMINUSER --password $ADMINPASS --firstname admin --lastname user --email ss@example.com
